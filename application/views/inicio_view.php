ecd39829c76834d889a68fae7e7c42084e47045f
maiko <maiko@debian>
2014-11-15 15:33:35 -0200

    Revert ""
    This reverts commit 4c72c917fb5566e58a3451dd047a4f9e155877df.

---
+++ application/views/inicio_view.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<title>Classificados</title>
+        
+        <link href="<?php echo base_url('bt/css/bootstrap.min.css');?>" rel="stylesheet">
+        <link href="<?php echo base_url('css/estilo.css');?>" rel="stylesheet">
+        
+        <script type="text/javascript" src="<?php echo base_url('js/jquery.js');?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('bt/js/bootstrap.min.js');?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('js/jquery.scrollTo.js');?>"></script>
+        
+</head>
+<body>
+    <!--Linha com tracos coloridos -->
+    <div class="container-fluid topo_cor">
+        <div class="row">
+          <div class="span4">
+              <img src="<?php echo base_url('imagens/cores_topo.gif');?>" id="cores_topo"/>
+          </div>
+        </div>
+    </div> 
+    <!--Banner e conteudo dele-->
+    <div class="container-fluid" style="background: #660000; background-image: url('imagens/background_banner.jpg'); background-size: 100% 100%; background-repeat: no-repeat;color: white;">
+        <div class="row">
+            
+            <span style="float: right; margin-top: 50px; margin-right: 100px"> Cadastre-se &nbsp;&nbsp;&nbsp;&nbsp;<button>Minha Conta</button></span>   
+
+            <div style="width: 100%; height: auto; border: 1px solid black;">
+                <div class="container">
+                    <div class="row" style="margin-left: 15px;">
+                        <img src="<?php echo base_url('imagens/logo.gif');?>"/><br>
+                        <form>
+                            <div style="margin: 0 auto 40px auto">
+                                <input type="text" placeholder="Busque um Servico ou Pessoa" style="width: 90%; height: 50px; margin-left: 15px; font-size: 20px; text-align: center"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                
+            </div>
+        </div>    
+    </div>
+    
+        <div class="col-md-12" style="margin-top: 10px">
+            <div class="col-md-4" style="height: auto;">
+                <h3 style="text-align: center">Categorias</h3>
+                
+               
+                    
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>Categoria x</h4>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <h4>+ Categorias</h4>
+                    </div><!-- /.col-lg-4 -->
+                          
+            </div>
+            
+            <div class="col-md-8" style="height: auto; background-image: url('imagens/degrade.jpg'); background-repeat: no-repeat; ">
+                <h3 style="text-align: center">Destaques</h3>
+                
+                <div class="row" >
+                    <div class="col-lg-4" style="margin-bottom: 10px">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4" style="margin-bottom: 10px">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4" style="margin-bottom: 10px">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
+                        <h4>Joao Carlos</h4>
+                        <h3> Diarista e Jardineiro</h3>
+                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
+                    </div><!-- /.col-lg-4 -->
+                    
+                </div>    
+            </div>
+        </div>    
+    
+    <!-- FOOTER -->
+    <footer style="text-align: center">
+        <hr/>
+        <p>Core D</p>
+        <p>&copy; 2014</p>
+    </footer>
+</body>
+</html>
