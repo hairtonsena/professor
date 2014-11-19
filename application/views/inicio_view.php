803a33761d2194a55eb2a66df0863ae7937f735b
maiko <maiko@debian>
2014-11-19 14:30:58 -0200

    Revert ""
    This reverts commit 4c72c917fb5566e58a3451dd047a4f9e155877df.

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -28,7 +28,7 @@
         <div class="col-md-12" style="margin-top: 10px">
             <div class="col-md-4" style="height: auto;">
                 <h3 style="text-align: center">Categorias</h3>
-                <div style="text-align: center">
+                <div style="text-align: center;">
                 <ul>
                     <li>Eletricista</li>
                     <li>Eletricista</li>
@@ -45,68 +45,66 @@
             
             <div class="col-md-8" style="height: auto; background-image: url('imagens/degrade.jpg'); background-repeat: no-repeat; ">
                 <h3 style="text-align: center">Destaques</h3>
-                
-                <div class="row" >
-                    <div class="col-lg-4" style="margin-bottom: 10px">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    <div class="col-lg-4" style="margin-bottom: 10px">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    <div class="col-lg-4" style="margin-bottom: 10px">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                </div>
-                <div class="row" >
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                </div>
-                <div class="row" >
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    <div class="col-lg-4">
-                        <img class="img-circle" src="imagens/websitePrefeirturaSaoJoaodaPonte.png" data-src="" height="140" alt="Generic placeholder image">
-                        <h4>Joao Carlos</h4>
-                        <h3> Diarista e Jardineiro</h3>
-                        <p><a class="btn btn-default" href="#" role="button">+detalhes</a></p>
-                    </div><!-- /.col-lg-4 -->
-                    
-                </div>    
+                <fieldset>
+            <br/><br/>
+            <table class="table table-hover">
+                <tbody>
+                        
+                        <tr>
+                            <td style="width: 180px">
+                                <img class="img-circle" src="#" data-src="" height="140" alt="Generic placeholder image">
+                            </td>
+                            <td>
+                                <h4>Deijane Graciele</h4><br/>TRABALHO EXCEPCIONAL!!!!!! ESTAMOS MUITO 
+                                SATISFEITOS COM NOSSO ÁLBUM.... UM LUXO!!! RECOMENDO SEMPRE!!!!!
+                            </td>                      
+                        </tr>
+                        <tr>
+                            <td style="width: 180px">
+                                <img class="img-circle" src="#" data-src="" height="140" alt="Generic placeholder image">
+                            </td>
+                            <td><h4>Debora Suelem</h4><br/>
+                                Vinicius Hoje quero te parabeniza pelo belissimo trabalho e te agradecer por tudo 
+                                que vc e sua equipe fizeram por mim ,e tornaram o momento mais especial da minha vida possivel um 
+                                grande abraço e nosso muito obrigrada.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 180px">
+                                <img class="img-circle" src="#" data-src="" height="140" alt="Generic placeholder image">
+                            </td>
+                            <td><h4>Cleonice Brito Oliveira</h4><br/>Trabalho lindo... e sem fazer força alguma.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 180px">
+                                <img class="img-circle" src="#" data-src="" height="140" alt="Generic placeholder image">
+                            </td>
+                            <td><h4>Graziele Fonseca</h4><br/>MAIS DO QUE FOTOS PEDAÇOS DE COISAS BOAS .....ADORO.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 180px">
+                                <img class="img-circle" src="#" data-src="" height="140" alt="Generic placeholder image">
+                            </td>
+                            <td><h4>Nátally Morais</h4><br/>muito lindo ameii
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 180px">
+                                <img class="img-circle" src="#" data-src="" height="140" alt="Generic placeholder image">
+                            </td>
+                            <td><h4>Maria Teresa</h4><br/>Muito obrigada!!! Meu álbum ficou perfeito!!!
+                            </td>
+                        </tr>
+
+                </tbody>
+            </table>
+
+             
+
+        </fieldset>
+                   
             </div>
         </div>    
     
