0b2292eab891de55f247fb7897a11c2c0dd97b4d
hairton <hairton@hairton-Lenovo-G460>
2014-11-21 14:47:36 -0200

    Banco de dado incrementado.

--- siteServico.sql
+++ siteServico.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 20-Nov-2014 às 00:48
+-- Data de Criação: 21-Nov-2014 às 16:45
 -- Versão do servidor: 5.5.37-0ubuntu0.13.10.1
 -- versão do PHP: 5.5.3-1ubuntu2.6
 
@@ -48,6 +48,60 @@ INSERT INTO `admin` (`id_admin`, `nome_admin`, `cpf_admin`, `email_admin`, `senh
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_categoria` varchar(35) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cidade`
+--
+
+CREATE TABLE IF NOT EXISTS `cidade` (
+  `id_cidade` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_cidade` varchar(30) NOT NULL,
+  `id_uf` int(3) NOT NULL,
+  PRIMARY KEY (`id_cidade`),
+  KEY `fk_uf` (`id_uf`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empreendimento`
+--
+
+CREATE TABLE IF NOT EXISTS `empreendimento` (
+  `id_empreendimento` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_empreendimento` varchar(70) NOT NULL,
+  `cpf_empreendimento` varchar(14) NOT NULL,
+  `cnpj_empreendimento` varchar(18) NOT NULL,
+  `endereco_empreendimento` varchar(70) NOT NULL,
+  `bairro_empreendimento` varchar(50) NOT NULL,
+  `telefone_empreendimento` varchar(15) NOT NULL,
+  `web_empreendimento` varchar(100) NOT NULL,
+  `descricao_empreendimento` text,
+  `logo_empreendimento` varchar(50) DEFAULT NULL,
+  `data_cadastro_empreendimento` date NOT NULL,
+  `status_empreendimento` int(1) NOT NULL,
+  `id_tipo_empreendimento` int(2) NOT NULL,
+  `id_sub_categoria` int(11) NOT NULL,
+  `id_cidade` int(11) NOT NULL,
+  PRIMARY KEY (`id_empreendimento`),
+  KEY `fk_tipo_empreendimento` (`id_tipo_empreendimento`),
+  KEY `fk_sub_categoria` (`id_sub_categoria`),
+  KEY `fk_cidade` (`id_cidade`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `privilegio_admin`
 --
 
@@ -64,6 +118,44 @@ CREATE TABLE IF NOT EXISTS `privilegio_admin` (
 INSERT INTO `privilegio_admin` (`id_privilegio_admin`, `nome_privilegio_admin`) VALUES
 (1, 'super');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sub_categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `sub_categoria` (
+  `id_sub_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_sub_categoria` varchar(35) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id_sub_categoria`),
+  KEY `fk_categoria` (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo_empreendimento`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_empreendimento` (
+  `id_tipo_empreendimento` int(2) NOT NULL,
+  `nome_tipo_empreendimento` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_tipo_empreendimento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `uf`
+--
+
+CREATE TABLE IF NOT EXISTS `uf` (
+  `id_uf` int(3) NOT NULL AUTO_INCREMENT,
+  `nome_uf` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_uf`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 --
 -- Constraints for dumped tables
 --
@@ -73,6 +165,26 @@ INSERT INTO `privilegio_admin` (`id_privilegio_admin`, `nome_privilegio_admin`) 
 --
 ALTER TABLE `admin`
   ADD CONSTRAINT `fk_privilegio_admin` FOREIGN KEY (`id_privilegio_admin`) REFERENCES `privilegio_admin` (`id_privilegio_admin`);
+
+--
+-- Limitadores para a tabela `cidade`
+--
+ALTER TABLE `cidade`
+  ADD CONSTRAINT `fk_uf` FOREIGN KEY (`id_uf`) REFERENCES `uf` (`id_uf`);
+
+--
+-- Limitadores para a tabela `empreendimento`
+--
+ALTER TABLE `empreendimento`
+  ADD CONSTRAINT `fk_cidade` FOREIGN KEY (`id_cidade`) REFERENCES `cidade` (`id_cidade`),
+  ADD CONSTRAINT `fk_sub_categoria` FOREIGN KEY (`id_sub_categoria`) REFERENCES `sub_categoria` (`id_sub_categoria`),
+  ADD CONSTRAINT `fk_tipo_empreendimento` FOREIGN KEY (`id_tipo_empreendimento`) REFERENCES `tipo_empreendimento` (`id_tipo_empreendimento`);
+
+--
+-- Limitadores para a tabela `sub_categoria`
+--
+ALTER TABLE `sub_categoria`
+  ADD CONSTRAINT `fk_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
