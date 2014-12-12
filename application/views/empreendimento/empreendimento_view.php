2df30d9040cbf8f2a591cb5a57f6cc6201746837
MOABBSOM QUALIDADE <MOABBSOM QUALIDADE@MOABBSOM>
2014-12-12 11:44:21 -0200

    12/12/2014 2º

--- application/views/empreendimento/empreendimento_view.php
+++ application/views/empreendimento/empreendimento_view.php
@@ -1,13 +1,13 @@
 <div class="col-md-10 posicao_conteiner" style="height: auto; background-repeat: no-repeat; ">
-    <h3 style="text-align: left;  margin-top: 50px; margin-bottom: 20px;">Destaques abc</h3>
+    <h3 style="text-align: left;  margin-top: 50px; margin-bottom: 20px;">Super Mercado do Zé</h3>
     <div class="row">
 
         <div class="col-sm-7 col-md-3 margen_baixo">
             <div class="thumbnail imagen_lista">
                 <img src="../imagen_teste.jpg">
                 <div class="caption alinha_direita">
-                    <h3>Thumbnail label</h3>
-                    <p>sdfasdf sdfsdfs sdfsadfsad asdfasdf asdfasdfasdf.</p>
+                    
+                    <p>sdfasdf sdfsdfs sdfsadfsad asdfasdf asdfasdfasdf sdfasdf sdfsdfs sdfsadfsad asdfasdf asdfasdfasdf.</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">Contato(s)</div>
                         <div class="panel-body">
